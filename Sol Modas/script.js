593c9c60a491b121588565a466ce48091ffc4fd8
Vinicius1241322412 <142948977+Vinicius1241322412@users.noreply.github.com>
2024-01-20 19:18:17 -0300

    sol5-login

--- Sol Modas/script.js
+++ Sol Modas/script.js
@@ -3,8 +3,10 @@ function ClicouMenu(){
     let fundo = document.getElementById('fundo')
     let p = document.getElementById('produtos2')
     var windowWidth = Number(window.innerWidth);
+    let simbolo = document.getElementById('left')
     var windowHeight = Number(window.innerHeight);
     if(navegar.style.width > '0vw'){
+        simbolo.style.transform = 'rotate(90deg)'
         p.style.maxHeight = '0vh'
         navegar.style.width = '0vw'
         fundo.style.backgroundColor = 'rgba(0, 0, 0, 0)'
@@ -22,9 +24,12 @@ function ClicouMenu(){
 }
 function ClicouProdutos(){
     let p = document.getElementById('produtos2')
+    let simbolo = document.getElementById('left')
     if(p.style.maxHeight == '0vh'){
+        simbolo.style.transform = 'rotate(-90deg)'
         p.style.maxHeight = '100vh';
     } else{
+        simbolo.style.transform = 'rotate(90deg)'
         p.style.maxHeight = '0vh'
     }
 }
