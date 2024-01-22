45cb7c9aa562228c434e97c4e42ebaed7f02bcd3
Vinicius1241322412 <142948977+Vinicius1241322412@users.noreply.github.com>
2024-01-22 20:25:21 -0300

    menu-final :(

--- Sol Modas/script.js
+++ Sol Modas/script.js
@@ -2,9 +2,7 @@ function ClicouMenu(){
     let navegar = document.getElementById('Nav')
     let fundo = document.getElementById('fundo')
     let p = document.getElementById('produtos2')
-    var windowWidth = Number(window.innerWidth);
     let simbolo = document.getElementById('left')
-    var windowHeight = Number(window.innerHeight);
     if(navegar.style.width > '0vw'){
         simbolo.style.transform = 'rotate(90deg)'
         p.style.maxHeight = '0vh'
@@ -13,11 +11,7 @@ function ClicouMenu(){
         fundo.style.width = '0vw'
 
     } else{
-        if(windowHeight < windowWidth){
-            navegar.style.width = '80vh';
-        } else{
-            navegar.style.width = '50vw';
-        }
+        navegar.style.width = '50vw';
         fundo.style.backgroundColor = 'rgba(0, 0, 0, 0.400)'
         fundo.style.width = '100vw'
     }
@@ -25,11 +19,11 @@ function ClicouMenu(){
 function ClicouProdutos(){
     let p = document.getElementById('produtos2')
     let simbolo = document.getElementById('left')
-    if(p.style.maxHeight == '0vh'){
-        simbolo.style.transform = 'rotate(-90deg)'
-        p.style.maxHeight = '100vh';
-    } else{
+    if(p.style.maxHeight == '100vh'){
         simbolo.style.transform = 'rotate(90deg)'
         p.style.maxHeight = '0vh'
+    } else{
+        simbolo.style.transform = 'rotate(-90deg)'
+        p.style.maxHeight = '100vh';
     }
 }
